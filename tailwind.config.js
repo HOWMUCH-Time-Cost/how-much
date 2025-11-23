7fe769d1ff523283b2fcbbfbea0a03dcd8d3420a
vincent-caetano <vincent.gcaetano@gmail.com>
2025-11-23 18:52:50 -0300

    Use Figtree as the font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Figtree', 'sans-serif'],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
